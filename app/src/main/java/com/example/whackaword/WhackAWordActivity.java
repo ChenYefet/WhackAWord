9b151a02955e1835f7706fcbc89db6f39180fa9b
ChenYefet <chenyefet@hotmail.co.uk>
2023-06-20 20:41:54 +0100

    Initial commit
    * Set up XML files, including those generated from SVG files

---
+++ app/src/main/java/com/example/whackaword/WhackAWordActivity.java
@@ -0,0 +1,15 @@
+package com.example.whackaword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhackAWordActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_whack_a_word);
+    }
+}
